bbe6fc7608a1b33aa5f33b833fd7e87c84f409bb
jailson santos <xjailsonsantosx@gmail.com>
2018-06-01 01:20:59 -0300

    melhorando sketch4

--- assets/sketchs/sketch4.js
+++ assets/sketchs/sketch4.js
@@ -1,8 +1,17 @@
 function setup() {
+  //funcao setup eh iniciada apenas uma vez
   createCanvas(480, 270);
+  background(100, 100, 100);
 }
 
 function draw() {
-  background(100, 100 , 100);
-  ellipse(50, 50, 25, 25);
+  //funcao draw eh repetida em loop infinito
+
+  // ellipse
+  fill(50, 50, 50);
+  ellipse(mouseX, 50, 25, 25);
+
+  // ellipse
+  fill(80, 80, 80);
+  rect(150, 150, 25, 25);
 }

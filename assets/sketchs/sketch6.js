7e09dd0a396d1525e4111ae554a894164e74d305
jailson santos <jailson_donald_cobain@hotmail.com>
2018-06-01 14:46:41 -0300

    trabalhando com declaracao if

--- assets/sketchs/sketch6.js
+++ assets/sketchs/sketch6.js
@@ -5,10 +5,23 @@ var canvasY = 400;
 function setup() {
   // funcao setup eh iniciada apenas uma vez
   createCanvas(canvasX, canvasY);
-  background(0);
 }
 
 function draw() {
   // funcao draw eh repetida em loop infinito
 
+  background(0);
+
+  stroke(255);
+  strokeWeight(2);
+  noFill();
+
+  if (mouseX > 300) {
+    fill(255, 0, 200);
+    // background(255);
+  }
+  // else {
+  //   background(0);
+  // }
+  ellipse(300, 200, 100, 100);
 }

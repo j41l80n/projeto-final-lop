d3423a732f5607bcb68035ebca6a0f95d951a2ce
jailson santos <xjailsonsantosx@gmail.com>
2018-06-01 08:44:49 -0300

    testes com variavel para movimentacao de objeto

--- assets/sketchs/sketch5.js
+++ assets/sketchs/sketch5.js
@@ -1,9 +1,18 @@
+// declarando a variavel circleX
+var circleX = 0;
+
 function setup() {
   // funcao setup eh iniciada apenas uma vez
   createCanvas(720, 480);
   background(205, 155, 55);
+
+  // inicializando a variavel circleX
+  circleX = 199;
 }
 
 function draw() {
   // funcao draw eh repetida em loop infinito
+
+  //usando a variavel definida inicialmente
+  ellipse(circleX, 100, 50, 50);
 }

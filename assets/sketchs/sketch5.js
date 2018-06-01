51ac347bd0a567e9d604faad8d8c0ca716a6d44b
jailson santos <jailson_donald_cobain@hotmail.com>
2018-06-01 10:37:17 -0300

    implementando a funcao random()

--- assets/sketchs/sketch5.js
+++ assets/sketchs/sketch5.js
@@ -1,19 +1,19 @@
 // variaveis
-
 // declarando e inicializando a variavel posicaoX com valor zero
 // var posicaoX = 0;
 // var posicaoY = 100;
 // var diametro = 50;
-var randomico = random(0, 50);
+var randomicoX = 0;
+var randomicoY = 0;
 var cor = 0;
 var canvasX = 600;
 var canvasY = 400;
 
 //definindo caracteristicas do personagem
 var personagem = {
-  posicaoX:100,
-  posicaoY:100,
-  diametro:90
+  posicaoX: 100,
+  posicaoY: 100,
+  diametro: 90
 };
 
 function setup() {
@@ -23,16 +23,23 @@ function setup() {
 }
 
 function draw() {
-// funcao draw eh repetida em loop infinito
+  // funcao draw eh repetida em loop infinito
+
+  // width para X
+  randomicoX = random(0, width);
+  // height para y
+  randomicoY = random(0, height);
 
   // variavel cor recebe o valor da posicao X do mouse
   cor = map(mouseX, 0, canvasX, 0, 255);
   //definindo cor de fundo
-  background(cor);
+  // background(cor);
 
   // usando as variaveis definidas previamente
-    ellipse(199, 100, 50, 50);
+  noStroke();
+  ellipse(randomicoX, randomicoY, 50, 50);
 
+  stroke(1);
   ellipse(mouseX, personagem.posicaoY, personagem.diametro, personagem.diametro);
   // incrementando a direcao de posicaoX para o deslocamento do personagem
   // posicaoX = posicaoX + 1;

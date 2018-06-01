7056850ec0ac69386ed15a2dee4dbaa65ff9b943
jailson santos <jailson_donald_cobain@hotmail.com>
2018-06-01 09:30:03 -0300

    modelando o objeto personagem

--- assets/sketchs/sketch5.js
+++ assets/sketchs/sketch5.js
@@ -1,26 +1,33 @@
 // variaveis
 
-// declarando e inicializando a variavel circleX com valor zero
-var circleX = 0;
+// declarando e inicializando a variavel posicaoX com valor zero
+// var posicaoX = 0;
+// var posicaoY = 100;
+// var diametro = 50;
+
+//definindo caracteristicas do personagem
+var personagem = {
+  posicaoX:0,
+  posicaoY:100,
+  diametro:60
+};
 
 function setup() {
   // funcao setup eh iniciada apenas uma vez
   createCanvas(720, 480);
   background(205, 155, 55);
-
-  // inicializando a variavel circleX com valor diferente de zero
-  circleX = 199;
 }
 
 function draw() {
-  // funcao draw eh repetida em loop infinito
+// funcao draw eh repetida em loop infinito
 
+  //definindo cor de fundo
   background(205, 155, 55);
 
-  // usando a variavel definida inicialmente
-  ellipse(circleX, 100, 50, 50);
+  // usando as variaveis definidas previamente
+  ellipse(199, 100, 50, 50);
 
-  // incrementando a direcao de circleX para o deslocamento do personagem
-  // circleX = circleX + 1;
-  circleX +=1;
+  // incrementando a direcao de posicaoX para o deslocamento do personagem
+  // posicaoX = posicaoX + 1;
+  //posicaoX += 1;
 }

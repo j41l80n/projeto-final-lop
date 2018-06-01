f543f2669559796074f11a8b1d50aa0baa88c76d
jailson santos <jailson_donald_cobain@hotmail.com>
2018-06-01 10:41:23 -0300

    iniciando sketch6

---
+++ assets/sketchs/sketch6.js
@@ -0,0 +1,12 @@
+// variaveis
+
+function setup() {
+  // funcao setup eh iniciada apenas uma vez
+  createCanvas(canvasX, canvasY);
+  background(0);
+}
+
+function draw() {
+  // funcao draw eh repetida em loop infinito
+
+}

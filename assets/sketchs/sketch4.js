c4b7f5ddbd3cfbdf597150d528da395c52c7694f
jailson santos <xjailsonsantosx@gmail.com>
2018-06-01 01:27:24 -0300

    utilizando a funcao mouseX e mouseY

--- assets/sketchs/sketch4.js
+++ assets/sketchs/sketch4.js
@@ -1,15 +1,18 @@
 function setup() {
-  //funcao setup eh iniciada apenas uma vez
+  // funcao setup eh iniciada apenas uma vez
   createCanvas(480, 270);
-  background(100, 100, 100);
+  // background(100, 100, 100);
 }
 
 function draw() {
-  //funcao draw eh repetida em loop infinito
+  // funcao draw eh repetida em loop infinito
+
+  // background(100, 100, 100);
 
   // ellipse
-  fill(50, 50, 50);
-  ellipse(mouseX, 50, 25, 25);
+  noStroke();
+  fill(50, 50, 50, 30);
+  ellipse(mouseX, mouseY, 25, 25);
 
   // ellipse
   fill(80, 80, 80);

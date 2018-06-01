b5ea3001327911b30c5a96b10af7de5087c396e5
jailson santos <jailson_donald_cobain@hotmail.com>
2018-06-01 08:57:57 -0300

    movendo personagem no eixo X

--- assets/sketchs/sketch5.js
+++ assets/sketchs/sketch5.js
@@ -1,4 +1,6 @@
-// declarando a variavel circleX
+// variaveis
+
+// declarando e inicializando a variavel circleX com valor zero
 var circleX = 0;
 
 function setup() {
@@ -6,13 +8,19 @@ function setup() {
   createCanvas(720, 480);
   background(205, 155, 55);
 
-  // inicializando a variavel circleX
+  // inicializando a variavel circleX com valor diferente de zero
   circleX = 199;
 }
 
 function draw() {
   // funcao draw eh repetida em loop infinito
 
+  background(205, 155, 55);
+
   // usando a variavel definida inicialmente
-  ellipse(circleX, 100, 50, 50); 
+  ellipse(circleX, 100, 50, 50);
+
+  // incrementando a direcao de circleX para o deslocamento do personagem
+  // circleX = circleX + 1;
+  circleX +=1;
 }

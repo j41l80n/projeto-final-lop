2cb639c393b2b33a1050db2f680eccf8bf9cfde3
jailson santos <jailson_donald_cobain@hotmail.com>
2018-06-02 14:33:31 -0300

    limpando codigo

--- assets/sketchs/etapa3.js
+++ assets/sketchs/etapa3.js
@@ -6,16 +6,29 @@ var posicaoX = 150;
 var posicaoY = 0;
 
 var personagem;
+var obstaculo;
 
 //definindo caracteristicas do personagem
 function Personagem() {
-  posicaoX = 100;
-  posicaoY = 200;
-  diametro = 50;
+  this.posicaoX = 100;
+  this.posicaoY = 200;
+  this.diametro = 50;
 
   this.criar = function() {
-		 // fill(this.r, this.g, this.b);
-     ellipse(posicaoX, posicaoY, diametro, diametro);
+    ellipse(this.posicaoX, this.posicaoY, this.diametro, this.diametro);
+  }
+};
+
+//definindo caracteristicas do obstaculo
+function Obstaculo() {
+  this.posicaoX = 450;
+  this.posicaoY = 200;
+  this.tamanhoX = 50;
+  this.tamanhoY = 50;
+
+  this.criar = function() {
+    rectMode(CENTER);
+    rect(this.posicaoX, this.posicaoY, this.tamanhoX, this.tamanhoY);
   }
 };
 
@@ -23,51 +36,41 @@ function setup() {
   // funcao setup eh iniciada apenas uma vez
   createCanvas(canvasX, canvasY);
   personagem = new Personagem();
+  obstaculo = new Obstaculo();
 }
 
 function draw() {
-  // funcao draw eh repetida em loop infinito
-
-  // definindo cor do background
   background(0);
-
-  // push();
-  // translate(width*0.5, height*0.5);
-  // rotate(frameCount / 50.0);
-  // star(10, 10, 5, 70, 5);
-  // pop();
 
   // faz personagem andar para esquerda quando seta do teclado pessionada
   if (keyIsDown(LEFT_ARROW)) {
-    personagem.posicaoX -=5;
+    personagem.posicaoX -= 5;
     posicaoX -= 5;
   }
 
   // faz personagem andar para direita quando seta do teclado pessionada
   if (keyIsDown(RIGHT_ARROW)) {
-    personagem.posicaoX +=5;
+    personagem.posicaoX += 5;
     posicaoX += 5;
   }
 
   // personagem
-  //ellipse(posicaoX, 200, 50, 50);
   personagem.criar();
+
   // obstaculo
-  // rectMode define o ponto central do retangulo
-  rectMode(CENTER);
-  rect(450, 200, 50, 50);
+  obstaculo.criar();
 }
 
 function star(x, y, radius1, radius2, npoints) {
   var angle = TWO_PI / npoints;
-  var halfAngle = angle/2.0;
+  var halfAngle = angle / 2.0;
   beginShape();
   for (var a = 0; a < TWO_PI; a += angle) {
     var sx = x + cos(a) * radius2;
     var sy = y + sin(a) * radius2;
     vertex(sx, sy);
-    sx = x + cos(a+halfAngle) * radius1;
-    sy = y + sin(a+halfAngle) * radius1;
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
     vertex(sx, sy);
   }
   endShape(CLOSE);

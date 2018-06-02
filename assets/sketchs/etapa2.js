4a1025a819472dacf1572b2b4c15e60edf99566d
jailson santos <jailson_donald_cobain@hotmail.com>
2018-06-02 11:50:02 -0300

    criando arquivo para etapa 2 e adicionando no index

--- assets/sketchs/etapa2.js
+++ assets/sketchs/etapa2.js
@@ -2,6 +2,9 @@
 var canvasX = 600;
 var canvasY = 400;
 
+var posicaoX = 150;
+var posicaoY = 0;
+
 function setup() {
   // funcao setup eh iniciada apenas uma vez
   createCanvas(canvasX, canvasY);
@@ -13,8 +16,15 @@ function draw() {
   // definindo cor do background
   background(0);
 
+  if (keyIsDown(LEFT_ARROW)) {
+    posicaoX -= 5;
+  }
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    posicaoX += 5;
+  }
   // personagem
-  ellipse(150, 200, 50, 50);
+  ellipse(posicaoX, 200, 50, 50);
 
   // obstaculo
   // rectMode define o ponto central do retangulo

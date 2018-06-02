771715e64568aadfe1aed7a1f6839594f77c6547
jailson santos <jailson_donald_cobain@hotmail.com>
2018-06-02 11:32:56 -0300

    finalizando etapa 1

--- assets/sketchs/etapa1.js
+++ assets/sketchs/etapa1.js
@@ -9,4 +9,15 @@ function setup() {
 
 function draw() {
   // funcao draw eh repetida em loop infinito
+
+  // definindo cor do background
+  background(0);
+
+  // personagem
+  ellipse(150, 200, 50, 50);
+
+  // obstaculo
+  // rectMode define o ponto central do retangulo
+  rectMode(CENTER);
+  rect(450, 200, 50, 50);
 }

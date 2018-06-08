f8015a60b54f45efd0f1c5e7c5fe03900e2aece9
jailson santos <jailson_donald_cobain@hotmail.com>
2018-06-08 10:53:21 -0300

    modulando codigo em funcoes

--- assets/sketchs/etapa6.js
+++ assets/sketchs/etapa6.js
@@ -7,11 +7,10 @@ var vidas = 3;
 var pontuacao = 0;
 var nivel = 1;
 var balas = 7;
-var ritgh = false;
 
 let tempoJogo = 10;
 let tempoAtirar = 3;
-var gameOVer = false;
+var fimJogo = false;
 var podeAtirar = true;
 var carregandoBalas = false;
 var personagem;
@@ -77,43 +76,16 @@ function setup() {
 }
 
 function draw() {
+  // define o backgroung para preto
   background(0);
-
+  // mostra posicao atual no personagem
   personagem.display();
-
-  fill(255, 255, 255);
-  text('Vidas: ' + vidas, 10, 30);
-  text('Balas: ' + balas, 90, 30);
-  text('Pontuação: ' + pontuacao, 170, 30);
-  text('Nivel: ' + nivel, 280, 30);
-
-  // faz personagem andar para esquerda quando seta do teclado pessionada
-  if (keyIsDown(LEFT_ARROW)) {
-    personagem.posicaoX -= 4;
-  }
-
-  // faz personagem andar para direita quando seta do teclado pessionada
-  if (keyIsDown(RIGHT_ARROW)) {
-    personagem.posicaoX += 4;
-    ritgh = true;
-  }
-
-  // obstaculos
-  for (var i = 0; i < obstaculoArray.length; i++) {
-    obstaculoArray[i].display();
-    obstaculoArray[i].posicaoY += random(-1, 1);
-    obstaculoArray[i].posicaoX -= obstaculoArray[i].velocidade;
-  }
-
-  // reseta a posicao do obstaculo
-  for (var i = 0; i < obstaculoArray.length; i++) {
-    if (obstaculoArray[i].posicaoX < -120) {
-      obstaculoArray[i].posicaoX = width;
-      obstaculoArray[i].posicaoY = 200;
-      obstaculoArray.push(new Obstaculo());
-      obstaculoArray[obstaculoArray.length - 1].velocidade += 1;
-    }
-  }
+  // realiza contador decressivo de tmepo
+  contagemRegressiva();
+  // movimentacao do Personagem
+  movimentacaoPersonagem();
+  // movimentacao dos Obstaculos
+  movimentacaoObstaculos();
 
   if (atirar == true) {
     for (var i = 0; i < balaArray.length; i++) {
@@ -122,34 +94,8 @@ function draw() {
     }
   }
 
-  if (frameCount % 60 == 0 && tempoJogo > 0) {
-    tempoJogo--;
-  }
-
-  fill(255, 255, 255);
-  text("Tempo: " + tempoJogo, 360, 30);
-
-  if (vidas == 0) {
-    noLoop();
-    fill(255, 255, 255);
-    text("G A M E  O V E R\n\tsem mais chances", width / 2, height * 0.8);
-    gameOVer = true;
-  }
-
-  if (tempoJogo == 0) {
-    noLoop();
-    fill(255, 255, 255);
-    text("G A M E  O V E R\n\tmorte por tempo", width / 2, height * 0.8);
-    gameOVer = true;
-  }
-
   if (balas == 0) {
     podeAtirar = false;
-  }
-
-  if (frameCount % 60 == 0 && tempoAtirar != 0 && podeAtirar == false) {
-    tempoAtirar--;
-    carregandoBalas = true;
   }
 
   if (carregandoBalas) {
@@ -173,12 +119,14 @@ function draw() {
   if (obstaculoArray.length == 0) {
     fill(255, 255, 255);
     text("P A R A B É N S\n\teliminação completa da ameaça", width / 2, height * 0.8);
-    gameOVer = true;
+    fimJogo = true;
     noLoop();
   }
 
+  indicadoresInformacao();
   colisaoBalaObstaculo();
   colisaoObstaculoPersonagem();
+  gameOVer();
 } // fim draw
 
 function mousePressed() {
@@ -188,11 +136,61 @@ function mousePressed() {
     balas--;
   }
 
-  if (gameOVer) {
+  if (fimJogo) {
     atirar = false;
     podeAtirar = false;
-    // tempoJogo = 15;
-    // loop();
+  }
+}
+
+function indicadoresInformacao() {
+  fill(255, 255, 255);
+  text('Vidas: ' + vidas, 10, 30);
+  text('Balas: ' + balas, 90, 30);
+  text('Pontuação: ' + pontuacao, 170, 30);
+  text('Nivel: ' + nivel, 280, 30);
+  text("Tempo: " + tempoJogo, 360, 30);
+}
+
+function contagemRegressiva() {
+  if (frameCount % 60 == 0 && tempoJogo > 0) {
+    tempoJogo--;
+  }
+
+  if (frameCount % 60 == 0 && tempoAtirar != 0 && podeAtirar == false) {
+    tempoAtirar--;
+    carregandoBalas = true;
+  }
+}
+
+function movimentacaoPersonagem() {
+  // faz personagem andar para esquerda quando seta do teclado pessionada
+  if (keyIsDown(LEFT_ARROW)) {
+    personagem.posicaoX -= 4;
+  }
+
+  // faz personagem andar para direita quando seta do teclado pessionada
+  if (keyIsDown(RIGHT_ARROW)) {
+    personagem.posicaoX += 4;
+  }
+}
+
+function movimentacaoObstaculos() {
+  for (var i = 0; i < obstaculoArray.length; i++) {
+    obstaculoArray[i].display();
+    obstaculoArray[i].posicaoY += random(-1, 1);
+    obstaculoArray[i].posicaoX -= obstaculoArray[i].velocidade;
+  }
+  resetaMovimentacaoObstaculo();
+}
+
+function resetaMovimentacaoObstaculo() {
+  for (var i = 0; i < obstaculoArray.length; i++) {
+    if (obstaculoArray[i].posicaoX < -120) {
+      obstaculoArray[i].posicaoX = width;
+      obstaculoArray[i].posicaoY = 200;
+      obstaculoArray.push(new Obstaculo());
+      obstaculoArray[obstaculoArray.length - 1].velocidade += 1;
+    }
   }
 }
 
@@ -213,10 +211,26 @@ function colisaoBalaObstaculo(){
 
 function colisaoObstaculoPersonagem() {
   for (var i = 0; i < obstaculoArray.length; i++) {
-    let hit = collideRectCircle(obstaculoArray[i].posicaoX + 10, obstaculoArray[i].posicaoY, 30, 30, personagem.posicaoX, personagem.posicaoY, 5);
+    let hit = collideRectCircle(obstaculoArray[i].posicaoX + 10, obstaculoArray[i].posicaoY, 30, 30, personagem.posicaoX, personagem.posicaoY, 20);
     if (hit) {
       vidas--;
       obstaculoArray.splice(i, 1);
     } // fim if
   } // fim for
+}
+
+function gameOVer() {
+  if (vidas == 0) {
+    noLoop();
+    fill(255, 255, 255);
+    text("G A M E  O V E R\n\tsem mais chances", width / 2, height * 0.8);
+    fimJogo = true;
+  }
+
+  if (tempoJogo == 0) {
+    noLoop();
+    fill(255, 255, 255);
+    text("G A M E  O V E R\n\tmorte por tempo", width / 2, height * 0.8);
+    fimJogo = true;
+  }
 }
